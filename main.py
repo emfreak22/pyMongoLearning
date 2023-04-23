30555af5dbf1b944797cc2dd3439ef8fa7cc809c
emfreak22 <emfreak22@gmail.com>
2023-04-24 04:16:41 +0530

    Added find

--- main.py
+++ main.py
@@ -17,11 +17,25 @@ class pyMongoClass():
         collection = self.db[table_name]
         collection.insert_many(data_list)
 
+    def find_one(self, find_dict, table_name = 'firstCollection'):
+        collection = self.db[table_name]
+        return collection.find_one(find_dict)
+
+    def find(self, find_dict, table_name = 'firstCollection'):
+        collection = self.db[table_name]
+        return [i for i in collection.find(find_dict)]
+
 if __name__ == '__main__':
+    db = pyMongoClass()
+
+    # insert one
     # data_dict = {'Name': 'Udit Singh', 'Age': 25}
-    # db = pyMongoClass()
     # db.insert_multiple_rows(table_name='firstCollection', data_dict = data_dict)
 
-    list_of_data = [{'_id':1,'Name':'Rahul','Age':21},{'_id':2,'Name':'Rohit','Age':22},{'_id':3,'Name':'Gopal','Age':20},{'_id':4,'Name':'Rahul Mann','Age':21}]
-    db = pyMongoClass()
-    db.insert_multiple_rows('firstCollection',list_of_data)
+    # insert many
+    # list_of_data = [{'_id':1,'Name':'Rahul','Age':21},{'_id':2,'Name':'Rohit','Age':22},{'_id':3,'Name':'Gopal','Age':20},{'_id':4,'Name':'Rahul Mann','Age':21}]
+    # db.insert_multiple_rows('firstCollection',list_of_data)
+
+    # find one
+    find_dict = {'Name': 'Rahul'}
+    print(db.find(find_dict=find_dict))
